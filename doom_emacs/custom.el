9a62f97f1e5f8076d1b1aadfa520d5590e84f0cc
Wojciech Matusiak <saphir2357@gmail.com>
2024-11-20 16:27:33 +0100

    Slightly modified DOOM default config

---
+++ doom_emacs/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ediff-fine-diff-A ((t (:extend t :background "#2257A0" :foreground "color-51" :weight bold))))
+ '(org-agenda-clocking ((t (:background "#262626")))))
+
